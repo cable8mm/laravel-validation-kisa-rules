7bb5ebd001e147d1ef739b788ebaf0be428931c4
Samgu Lee <cable8mm@gmail.com>
2024-03-05 00:21:06 +1300

    Update the EditorConfig (#8)

--- tests/Rules/KisaPasswordTest.php
+++ tests/Rules/KisaPasswordTest.php
@@ -3,30 +3,53 @@
 namespace Cable8mm\ValidationKisaRules\Tests\Rules;
 
 use Cable8mm\ValidationKisaRules\Rules\KisaPassword;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase;
 
 class KisaPasswordTest extends TestCase
 {
-    public function test_kisa_password_rule()
+    use WithWorkbench;
+
+    protected $enablesPackageDiscoveries = true;
+
+    protected function getPackageProviders($app)
     {
-        // Arrange
-        // 문자,숫자,특수 문자 세가지 중 2가지 이상 8자-20자 또는 세가지 중 1가지 이상 10자-20자 입니다.
-        $kisaPasswordRule = new KisaPassword();
+        return [
+            'Illuminate\Translation\TranslationServiceProvider',
+        ];
+    }
+
+    public function test_pass_according_to_kisa_password_rule_or_not()
+    {
+        $rule = new KisaPassword();
 
         $passwords = [
             '12345678' => false,
             'abcdefgha' => false,
+            // Pass Condition 1
+            '12345678232342' => true,
+            // Pass Condition 1
+            'skdfjoaweijfae' => true,
+            // Pass Condition 2
             '12345678a' => true,
+            // Pass Condition 2
             '123asd!!' => true,
-
+            'ijiasdfj2@fjdsk' => true,
         ];
 
-        // Act
         foreach ($passwords as $password => $expected) {
-            $actual = $kisaPasswordRule->passes('attribute', $password);
+            $actual = $rule->passes('attribute', $password);
 
-            // Assert
             $this->assertEquals($expected, $actual, $password);
         }
+    }
+
+    public function test_validate_message_can_be_seen()
+    {
+        $rule = new KisaPassword();
+
+        $actual = $rule->passes('attribute', '1');
+
+        $this->assertEquals('validationKisaRules::messages.kisa_password', $rule->message());
     }
 }
